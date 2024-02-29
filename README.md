1420c232523644cd290627ab545bbf02f3aa895d
zalanderwastaken <muditmishra.mr@gmail.com>
2024-02-29 19:52:32 +0530

    Small update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 A simple OS/Wrapper made for computer craft to make it easier for new people to start programming.
 Download (Copy paste the installer.lua) into the computer that you want to insall Door OS on and run the file.
+::UPDATES::
++Added newer installer
++Made startup.lua main menu better
++Removed viminstaller.lua from repair.lua
---
+++ installationfiles/installer.lua
@@ -0,0 +1,55 @@
+print("Install Door OS ?")
+inp = read():lower()
+if inp == "n" then
+    goto exit
+end
+urls = {"https://raw.githubusercontent.com/zalanwastaken/Door-os/main/startup.lua", "https://raw.githubusercontent.com/zalanwastaken/Door-os/main/verify.lua", "https://raw.githubusercontent.com/zalanwastaken/Door-os/main/cash.lua", "https://raw.githubusercontent.com/zalanwastaken/Door-os/main/os/doorcli.lua", "https://raw.githubusercontent.com/zalanwastaken/Door-os/main/os/filemgr.lua", "https://raw.githubusercontent.com/zalanwastaken/Door-os/main/os/programmanger.lua", "https://raw.githubusercontent.com/zalanwastaken/Door-os/main/repair.lua"}
+files = {"startup.lua", "verify.lua", "cash.lua", "/os/doorcli.lua", "/os/filemgr.lua", "/os/programmanager.lua", "repair.lua"}
+shell.run("mkdir /os")
+shell.run("mkdir /os/data")
+shell.run("mkdir /os/programs")
+shell.run("cp /rom/programs/fun/hello.lua /os/programs")
+shell.run("cp /rom/programs/fun/worm.lua /os/programs")
+function download(url, file, noerr)
+    local content = http.get(url)
+    if not content then
+        if not noerr then
+            error("Failed to access resource " .. url)
+        else
+            return false
+        end
+    end
+    print("Got data "..url)
+    content = content.readAll()
+    local fi = fs.open(file, "w")
+    fi.write(content)
+    fi.close()
+    print("Dowloaded "..url)
+    return true
+end
+function error()
+    while true do
+        print("INSTALLATION FAILED EXIT ? Y/N")
+        inp = read():lower()
+        if inp == "Y" then
+            shell.run("shutdown")
+        end
+    end
+end
+for i = 1, #urls, 1 do
+    if not(download(urls[i], files[i], true)) then
+        error()
+    end
+end
+if not(download("https://raw.githubusercontent.com/zalanwastaken/Door-os/main/installationfiles/viminstaller.lua", "/viminstaller.lua", true)) then
+    error()
+end
+shell.run("/viminstaller.lua")
+shell.run("rm viminstaller.lua")
+shell.run("/repair.lua")
+print("Reboot now ?")
+inp = read():lower()
+if inp == "y" then
+    shell.run("reboot")
+end
+:: exit ::
